1bc42eb6741b4ae5773effcbb22edcfbf576d34a
aaroexxt <aaron.becker@gmail.com>
2021-01-01 21:00:59 -0700

    done w trip

---
+++ assets/js/A_Bar.js
@@ -0,0 +1,189 @@
+/*
+Weird bar graph thing library
+Written by Aaron Becker
+*/
+
+var A_Bar = function(canvasID, opts) {
+	this.canvas = document.getElementById(canvasID);
+	this.ctx = this.canvas.getContext('2d');
+	this.options = { //setup default opts object
+
+		/* BASIC PARAMS (width, height, etc) */
+		x: 0,
+		y: 0,
+		width: 30, //px
+		height: 100, //px
+		horizontal: true,
+		drawOuterBox: true,
+		initialPaint: true,
+
+		/* MIN/MAX */
+		min: 0,
+		max: 100,
+		startColor: "#f00", //html color
+		endColor: "#0f0", //html color
+
+		/* OFFSETS */
+		sideOffset: 2, //px
+
+		/* DRAW OPTIONS (stroke width, default color, etc) */
+		strokeWidth: 1, //px
+		tickWidth: 3, //px
+		fontSize: 12, //px (10 big)
+		units: "V",
+		title: "",
+		bgColor: "#002", //html color
+		strokeColor: "#ddd" //html color
+	}
+
+
+	//Now, we loop through opts to see if we should override any of the default options
+	let keys = Object.keys(this.options);
+	for (let i=0; i<keys.length; i++) {
+		if (opts.hasOwnProperty(keys[i])) { //does it exist?
+			if (typeof opts[keys[i]] != "undefined") { //does it exist pt 2?
+				this.options[keys[i]] = opts[keys[i]]; //override it!
+			}
+		}
+	}
+
+	//By default, set horizontal var by aspect ratio (but can be overridden)
+	this.options.horizontal = (this.options.width > this.options.height);
+
+	this.startColorRGB = hexToRgb(this.options.startColor);
+	this.endColorRGB = hexToRgb(this.options.endColor);
+
+	if (this.options.initialPaint) this.update((this.options.min+this.options.max)/2)
+}
+
+
+A_Bar.prototype.drawOuterBox = function(x, y, width, height, radius) {
+	this.ctx.strokeStyle = this.options.strokeColor;
+	this.ctx.fillStyle = this.options.strokeColor;
+	this.ctx.lineWidth = this.options.strokeWidth;
+
+	if (typeof radius === 'number') {
+		radius = {tl: radius, tr: radius, br: radius, bl: radius};
+	} else {
+		var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+		for (var side in defaultRadius) {
+		  radius[side] = radius[side] || defaultRadius[side];
+		}
+	}
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(x + radius.tl, y);
+	this.ctx.lineTo(x + width - radius.tr, y);
+	this.ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr); //bruh for some reason you have to use curves for this lmao
+	this.ctx.lineTo(x + width, y + height - radius.br);
+	this.ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+	this.ctx.lineTo(x + radius.bl, y + height);
+	this.ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+	this.ctx.lineTo(x, y + radius.tl);
+	this.ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+	this.ctx.closePath();
+	this.ctx.stroke();
+}
+
+A_Bar.prototype.update = function(value) {
+	let text = this.options.title+String(value)+this.options.units;
+	value = constrain(value, this.options.min, this.options.max); //constrain after text is made
+	this.ctx.font = this.options.fontSize+"px Helvetica";
+	let tWidth = this.ctx.measureText(text).width+this.options.strokeWidth*3;
+	let tHeight = this.options.fontSize+this.options.strokeWidth*3;
+
+	let twDiv2 = this.options.tickWidth/2;
+
+	//Clear background
+	this.ctx.fillStyle = this.options.bgColor;
+	this.ctx.fillRect(this.options.x, this.options.y, this.options.width, this.options.height);
+
+	//Draw background (in 1px strips)
+	let color = this.startColorRGB;
+	if (this.options.horizontal) {
+		let pxCenter = map(value, this.options.min, this.options.max, twDiv2+tWidth, this.options.width-twDiv2);
+		let barWidth = (this.options.width-tWidth);
+
+		let diff = { //difference by pixel
+			r: (this.endColorRGB.r-this.startColorRGB.r)/barWidth,
+			g: (this.endColorRGB.g-this.startColorRGB.g)/barWidth,
+			b: (this.endColorRGB.b-this.startColorRGB.b)/barWidth
+		}
+		for (let i=tWidth; i<this.options.width; i++) { //fill it in 1px at a time
+			this.ctx.fillStyle = rgbToHex(color.r, color.g, color.b);
+			this.ctx.fillRect(i+this.options.x, this.options.sideOffset+this.options.y, 1, this.options.height-(2*this.options.sideOffset));
+			color.r += diff.r;
+			color.g += diff.g;
+			color.b += diff.b;
+		}
+
+		if (this.options.drawOuterBox) {
+			this.drawOuterBox(this.options.strokeWidth+tWidth+this.options.x, this.options.strokeWidth+this.options.y, this.options.width-(2*this.options.strokeWidth)-tWidth, this.options.height-(2*this.options.strokeWidth), 2);
+		}
+
+		this.ctx.fillStyle = this.options.strokeColor;
+		this.ctx.fillRect(pxCenter-twDiv2+this.options.x, this.options.y, this.options.tickWidth, this.options.height);
+
+		this.ctx.fillText(text, this.options.x, (this.options.height+tHeight)/2-this.options.strokeWidth+this.options.y);
+	} else {
+		let pxCenter = map(value, this.options.min, this.options.max, twDiv2+tHeight, this.options.height-twDiv2);
+
+		let barHeight = (this.options.height-tHeight);
+		let diff = { //difference by pixel
+			r: (this.endColorRGB.r-this.startColorRGB.r)/barHeight,
+			g: (this.endColorRGB.g-this.startColorRGB.g)/barHeight,
+			b: (this.endColorRGB.b-this.startColorRGB.b)/barHeight
+		}
+		for (let i=tHeight; i<this.options.height; i++) {
+			this.ctx.fillStyle = rgbToHex(color.r, color.g, color.b);
+			this.ctx.fillRect(this.options.sideOffset+this.options.x, i+this.optionx.y, this.options.width-(2*this.options.sideOffset), 1);
+			color.r += diff.r;
+			color.g += diff.g;
+			color.b += diff.b;
+		}
+
+		if (this.options.drawOuterBox) {
+			this.drawOuterBox(this.options.strokeWidth+this.options.x, this.options.strokeWidth+tHeight+this.options.y, this.options.width-(2*this.options.strokeWidth), this.options.height-(2*this.options.strokeWidth)-tHeight, 2);
+		}
+
+		this.ctx.fillStyle = this.options.strokeColor;
+		this.ctx.fillRect(this.options.x, pxCenter-twDiv2+this.options.y, this.options.width, this.options.tickWidth);
+
+		this.ctx.fillText(text, (((this.options.width-tWidth)/2)+this.options.strokeWidth*2)+this.options.x, this.options.fontSize+this.options.y);
+	}
+}
+
+function hexToRgb(hex) {
+	if (hex.length == 3 || hex.length == 4) {
+		let fHex = "#";
+		for (i=(hex[0] == "#")?1:0; i<hex.length; i++) {
+			fHex+=hex[i];
+			fHex+=hex[i];
+		}
+		hex = fHex;
+	}
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result ? { //NUTTY expression here
+		r: parseInt(result[1], 16),
+		g: parseInt(result[2], 16),
+		b: parseInt(result[3], 16)
+	} : null;
+}
+
+function componentToHex(c) {
+  var hex = Math.floor(c).toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function map(number, in_min, in_max, out_min, out_max) {
+	var val = (number - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	return val > out_max ? out_max : val < out_min ? out_min : val;
+}
+
+function constrain(number, min, max) {
+	return number > max ? max : number < min ? min : number;
+}
--- assets/js/A_StateIndicator.js
+++ assets/js/A_StateIndicator.js
@@ -4,18 +4,21 @@ Written by Aaron Becker
 */
 
 var A_StateIndicator = function(canvasID, opts) {
+	this.canvasID = canvasID;
 	this.canvas = document.getElementById(canvasID);
 	this.ctx = this.canvas.getContext('2d');
 	this.options = { //setup default opts object
 		/* BASIC PARAMS (width, height, etc) */
 		width: 350, //px
-		height: 225, //px
+		height: 150, //px
+		heightBanner: 50,
+		dataColHeight: 30,
 		fpsUpdate: 30,
 
 		/* DRAW OPTIONS (stroke width, default color, etc) */
 		strokeWidth: 1, //px
-		titleFontSize: 75, //px (15 big)
-		timeFontSize: 13,
+		titleFontSize: 30, //px (15 big)
+		dataFontSize: 12,
 		bgColor: "#000", //html color
 		strokeColor: "#ddd", //html color
 		titleColor: "#a00",
@@ -39,10 +42,188 @@ var A_StateIndicator = function(canvasID, opts) {
 
 	this.construct();
 
-	this.updateInterval = setInterval(() => {this.update();}, 1000/this.options.fpsUpdate);
+	this.data = [
+		["battV", "AVI Batt", "V", 0],
+		["state", "Sys State", "", 0],
+
+		["rollV", "RollReg", "V", 0],
+		["pyroState", "Pyro State", "", false],
+
+		["servoV", "ServoReg", "V", 0],
+		["temp", "Board T", "°F", 0],
+
+		["signal", "SigStrength", "dbm", 0],
+		["tlmRate", "TLM Rate", "p/sec", 0],
+
+		["pDOP", "pDOP", "", 0],
+		["gpsSats", "GPS Sats", "", 0],
+
+		["horizAcc", "UNCHoriz", "m", 0],
+		["vertAcc", "UNCVert", "m", 0]
+	]
+
+	let barWidth = this.options.width/2.2;
+	let barHeight = (this.options.height-this.options.heightBanner)/(this.data.length/2);
+
+
+	console.log(barWidth, barHeight)
+	this.battVBar = new A_Bar(this.canvasID, {
+		width: barWidth,
+		height: barHeight,
+		x: this.getBarX("battV"),
+		y: this.getBarY("battV"),
+		fontSize: this.options.dataFontSize,
+		min: 9.6,
+		max: 12.6,
+		units: this.getBarUnits("battV")
+	});
+	this.rollVBar = new A_Bar(this.canvasID, {
+		width: barWidth,
+		height: barHeight,
+		x: this.getBarX("rollV"),
+		y: this.getBarY("rollV"),
+		fontSize: this.options.dataFontSize,
+		min: 5.8,
+		max: 6,
+		units: this.getBarUnits("rollV")
+	});
+	this.servoVBar = new A_Bar(this.canvasID, {
+		width: barWidth,
+		height: barHeight,
+		x: this.getBarX("servoV"),
+		y: this.getBarY("servoV"),
+		fontSize: this.options.dataFontSize,
+		min: 8,
+		max: 8.4,
+		units: this.getBarUnits("servoV")
+	});
+	this.sigBar = new A_Bar(this.canvasID, {
+		width: barWidth,
+		height: barHeight,
+		x: this.getBarX("signal"),
+		y: this.getBarY("signal"),
+		fontSize: this.options.dataFontSize,
+		min: -90,
+		max: -20,
+		units: this.getBarUnits("signal")
+	});
+	this.pdopBar = new A_Bar(this.canvasID, {
+		width: barWidth,
+		height: barHeight,
+		x: this.getBarX("pDOP"),
+		y: this.getBarY("pDOP"),
+		fontSize: this.options.dataFontSize,
+		min: 6,
+		max: 1.25,
+		units: this.getBarUnits("pDOP")
+	});
+
+	this.construct();
+
+
+	// (battV, rollV, servoV, state, pyroState, temp, signal, tlmRate, fixType, pDOP, horizAcc, vertAcc, gpsSats)
+
+	//this.updateInterval = setInterval(() => {this.update();}, 1000/this.options.fpsUpdate);
 }
 
 A_StateIndicator.prototype.clear = function() {
-
+	this.ctx.fillStyle = this.options.bgColor;
+	this.ctx.fillRect(0, 0, this.options.width, this.options.height);
 }
-A_StateIndicator.prototype.construct = function()
+
+A_StateIndicator.prototype.update = function(data) {
+	if (data.length == 0) return;
+
+	let dKeys = Object.keys(data);
+
+	let redraw = false;
+	for (let i = 0; i<dKeys.length; i++) {
+		for (let j=0; j<this.data.length; j++) {
+			if (this.data[j][0] == dKeys[i]) {
+				this.data[j][3] = data[dKeys[i]];
+				redraw = true;
+				break; //break out of inner loop
+			}
+		}
+	}
+
+	if (redraw) this.construct();
+}
+
+
+A_StateIndicator.prototype.drawOuterBox = function(x, y, width, height, radius) {
+	this.ctx.strokeStyle = this.options.strokeColor;
+	this.ctx.fillStyle = this.options.strokeColor;
+	this.ctx.lineWidth = this.options.strokeWidth;
+
+	if (typeof radius === 'number') {
+		radius = {tl: radius, tr: radius, br: radius, bl: radius};
+	} else {
+		var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+		for (var side in defaultRadius) {
+		  radius[side] = radius[side] || defaultRadius[side];
+		}
+	}
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(x + radius.tl, y);
+	this.ctx.lineTo(x + width - radius.tr, y);
+	this.ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr); //bruh for some reason you have to use curves for this lmao
+	this.ctx.lineTo(x + width, y + height - radius.br);
+	this.ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+	this.ctx.lineTo(x + radius.bl, y + height);
+	this.ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+	this.ctx.lineTo(x, y + radius.tl);
+	this.ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+	this.ctx.closePath();
+	this.ctx.stroke();
+}
+
+A_StateIndicator.prototype.getBarX = function(name) {
+	for (let i=0; i<this.data.length; i++) {
+		if (this.data[i][0] == name) {
+			return 5+(i%2)*(this.options.width/2);
+		}
+	}
+	return 0;
+}
+
+A_StateIndicator.prototype.getBarY = function(name) {
+	for (let i=0; i<this.data.length; i++) {
+		if (this.data[i][0] == name) {
+			let barHeight = (this.options.height-this.options.heightBanner)/(this.data.length/2);
+			return this.options.heightBanner+(barHeight*(i/2));
+		}
+	}
+	return 0;
+}
+
+A_StateIndicator.prototype.getBarUnits = function(name) {
+	for (let i=0; i<this.data.length; i++) {
+		if (this.data[i][0] == name) {
+			return this.data[i][2];
+		}
+	}
+	return "";
+}
+
+A_StateIndicator.prototype.drawDivLine = function() {
+	this.ctx.strokeStyle = this.options.strokeColor;
+	this.ctx.fillStyle = this.options.strokeColor;
+	this.ctx.lineWidth = this.options.strokeWidth;
+
+	this.ctx.beginPath();
+	this.ctx.moveTo(this.options.width/2, this.options.height-this.options.strokeWidth);
+	this.ctx.lineTo(this.options.width/2, this.options.heightBanner+10);
+	this.ctx.stroke();
+}
+
+
+A_StateIndicator.prototype.construct = function() {
+	// this.clear();
+	this.drawOuterBox(this.options.strokeWidth, this.options.strokeWidth, this.options.width-(2*this.options.strokeWidth), this.options.height-(2*this.options.strokeWidth), 10);
+	this.drawDivLine();
+
+	// this.drawStateBanner();
+	let x = 0;
+}
